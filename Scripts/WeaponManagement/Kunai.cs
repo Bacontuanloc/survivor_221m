5cfa01b88bdfc7180d087a3d163a4a0a1d388fe6
quanqkm <minhquangkhong3006@gmail.com>
2023-03-19 02:19:20 +0700

    weapon structure

---
+++ Scripts/WeaponManagement/Kunai.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.WeaponManagement
+{
+    public class Kunai : MonoBehaviour, IWeapon
+    {
+        public GameObject bulletPrefab;
+        public float bulletSpeed = 10f;
+        public float fireRate = 0.5f;
+        public float damage = 5f;
+        public void Shoot()
+        {
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            Vector3 size = renderer.bounds.size;
+            Vector3 localTopRight = new Vector3(size.x / 2, size.y / 2, 0);
+            Vector3 worldTopRight = gameObject.transform.TransformPoint(localTopRight);
+            Vector3 localTopLeft = new Vector3(-size.x / 2, size.y / 2, 0);
+            Vector3 worldTopLeft = gameObject.transform.TransformPoint(localTopLeft);
+            GameObject bullet = BulletPool.SharedInstance.GetPooledObject();
+            if (bullet != null)
+            {
+                bullet.transform.position = (worldTopLeft + worldTopRight) / 2;
+                bullet.transform.rotation = gameObject.transform.rotation;
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(gameObject.transform.up * bulletSpeed, ForceMode2D.Impulse);
+            }
+        }
+
+        public void ExecuteSkill()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Scripts/WeaponManagement/WeaponFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.WeaponManagement
+{
+    public class WeaponFactory : MonoBehaviour
+    {
+        public GameObject InstantiateWeapon(WeaponType weaponType)
+        {
+            GameObject weapon;
+            switch (weaponType)
+            {
+                case WeaponType.Pistol:
+                    var pistol = Resources.Load("Prefabs/Gun") as GameObject;
+                    weapon = Instantiate(pistol);
+                    return weapon;
+                case WeaponType.Kunai:
+                    var kunai = Resources.Load("Prefabs/Gun") as GameObject;
+                    weapon = Instantiate(kunai);
+                    return weapon;
+                case WeaponType.Bat:
+                    var bat = Resources.Load("Prefabs/Gun") as GameObject;
+                    weapon = Instantiate(bat);
+                    return weapon;
+                default:
+                    return null;
+            }
+        }
+    }
+}
