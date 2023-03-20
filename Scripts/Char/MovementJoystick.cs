c3d846436e597c5e37ce9da3c7ce56574c6ad87e
nhhainam <nhhainam@gmail.com>
2023-03-20 23:45:03 +0700

    Implement joystick for characters

---
+++ Scripts/Char/MovementJoystick.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Char
+{
+    public class MovementJoystick : MonoBehaviour
+    {
+        public GameObject joystick;
+        public GameObject joystickBG;
+        public Vector2 joystickVec;
+        public Vector2 joystickTouchPos;
+        public Vector2 joystickOriginPos;
+        private float joystickRadius;
+
+        private void Start()
+        {
+            joystickOriginPos = joystickBG.transform.position;
+            joystickRadius = joystickBG.GetComponent<RectTransform>().sizeDelta.y / 4;
+        }
+
+        public void PointerDown()
+        {
+            joystick.transform.position = Input.mousePosition;
+            joystickBG.transform.position = Input.mousePosition;
+            joystickTouchPos = Input.mousePosition;
+        }
+
+        public void PointerUp()
+        {
+            joystickVec = Vector2.zero;
+
+            joystick.transform.position = joystickOriginPos;
+            joystickBG.transform.position = joystickOriginPos;
+        }
+
+        public void Drag(BaseEventData baseEventData)
+        {
+            PointerEventData pointerEventData = baseEventData as PointerEventData;
+            Vector2 dragPos = pointerEventData.position;
+            joystickVec = (dragPos - joystickTouchPos).normalized;
+
+            float joystickDist = Vector2.Distance(dragPos, joystickTouchPos);
+
+            if (joystickDist < joystickRadius)
+            {
+                joystick.transform.position = joystickTouchPos + joystickVec * joystickDist;
+            }
+
+            else
+            {
+                joystick.transform.position = joystickTouchPos + joystickVec * joystickRadius;
+            }
+        }
+    }
+}
--- Scripts/Char/Tomee.cs
+++ Scripts/Char/Tomee.cs
@@ -15,43 +15,6 @@ namespace Assets.Scripts.Char
             health = 20f;
             rotateSpeed = 500f;
         }
-        private void Start()
-        {
-            currentState = new NormalState(this);
-        }
-        private void Update()
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-
-            Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
-            float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
-            movementDirection.Normalize();
-            transform.Translate(movementDirection * speed * Time.deltaTime * inputMagnitude, Space.World);
-            if (movementDirection != Vector2.zero)
-            {
-                Quaternion toRoation = Quaternion.LookRotation(Vector3.forward, movementDirection);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRoation, rotateSpeed * Time.deltaTime);
-            }
-            currentState.UpdateState(this);
-        }
-
-        public override void ChangeState(CharacterState newState)
-        {
-            if (currentState != null)
-            {
-                currentState.ExitState(this);
-            }
-
-            currentState = newState;
-            currentState.EnterState(this);
-        }
-
-        public override void Move()
-        {
-            throw new NotImplementedException();
-        }
-
         public override void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Enemy"))
