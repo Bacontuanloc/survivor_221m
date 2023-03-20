fc40b2b24ae3d029ac08731e0838e6cabf56480a
linhle1512 <98157359+linhle1512@users.noreply.github.com>
2023-03-20 15:06:02 +0700

    update background and sound

--- Scripts/BaseSceneScript/PauseGame.cs
+++ Scripts/BaseSceneScript/PauseGame.cs
@@ -9,7 +9,7 @@ public class PauseGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
@@ -21,7 +21,7 @@ public class PauseGame : MonoBehaviour
     public void StopGame()
     {
         Time.timeScale = 0;
-        pausePanel.SetActive(true);
+        pausePanel.SetActive(true);       
     }
 
     public void ContinueGame()
