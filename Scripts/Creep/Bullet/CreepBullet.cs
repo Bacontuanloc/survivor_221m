4adf199a419001473d1bfb8ae7df660a3affc08f
Bacontuanloc <thinhvdhe150056@fpt.edu.vn>
2023-03-21 07:55:37 +0700

    Commit bullet

--- Scripts/Creep/Bullet/CreepBullet.cs
+++ Scripts/Creep/Bullet/CreepBullet.cs
@@ -9,7 +9,7 @@ public class CreepBullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 5);
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
