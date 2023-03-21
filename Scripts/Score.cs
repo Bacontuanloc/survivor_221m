28f93ca2b6c7eda4e7d60b10196243d704caa5ad
nhhainam <nhhainam@gmail.com>
2023-03-21 21:18:25 +0700

    Implement observer pattern for update score

---
+++ Scripts/Score.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public int scoreNum;
+    public Text enemiesDestroyedText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreNum = 0;
+        enemiesDestroyedText = GameObject.FindWithTag("Score").GetComponent<Text>();
+    }
+
+    public void updateScore(bool hit)
+    {
+        if (hit)
+        {
+            scoreNum += 1;
+            enemiesDestroyedText.text = "Enemy Destroyed" + scoreNum;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
